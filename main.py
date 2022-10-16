a5e740adf0cf88ef7cbffc0e68a0fd2ef61c65c0
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-10-15 21:59:41 -0700

    created layout styles and register pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.secret_key = 'abubakar'
@@ -6,8 +6,12 @@ app.secret_key = 'abubakar'
 
 @app.route('/')
 def login():
-
     return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
